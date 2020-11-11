468f2074723992289709162acce5b42ce205f0a4
Wantana  Wankhum <614259045@webmail.npru.ac.th>
2020-11-11 11:42:53 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = {
+    "name": "Wantana  Wankhum",
+    "aboutme":{
+        "Nickname":"Waew",
+        "Phone":"092/7788086",
+        "Email":"Waw.010158@gmail.com",
+        "Address":"31/5, Sam Ngam , Dontum , Nakhon Pathom 73150",
+    
+    },
+    "education":{
+        "Upper":"Kongthong wittaya",
+        "lowers":"Kongthong wittaya"
+    },
+   
+};
+module.exports = data;
